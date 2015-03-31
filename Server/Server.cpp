b8c17633beda890a368e20f8be1f273641e81d5c
sspeaks <sspeaks610@tamu.edu>
2015-03-31 17:18:11 -0500

    Server code de-commented

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>              // for printf
-#include <winsock2.h>   // the networking library
-#pragma comment(lib, "ws2_32.lib")      // links WinSock2 with MS Visual Studio
-#include <vector>       // for the server's client list and input buffer
-#include <conio.h>      // for kbhit() and getch()
+#include <stdio.h>              
+#include <winsock2.h>   
+#pragma comment(lib, "ws2_32.lib")      
+#include <vector>       
+#include <conio.h>      
 
 int serverLogic(SOCKET mySocket, sockaddr * connectionAddress);
 int clientLogic(SOCKET mySocket, const sockaddr* connectionAddr);
@@ -14,62 +14,40 @@ int main()
 {
 	sockaddr_in connectionAddress;
 	unsigned short myPort = 1337;
-	// initialize the socket's address
-	memset(&connectionAddress, 0, sizeof(sockaddr_in)); // initialize to zero
+	
+	memset(&connectionAddress, 0, sizeof(sockaddr_in)); 
 	connectionAddress.sin_family = AF_INET;
-	// host-to-network-short: big-endian conversion of a 16 byte value
+	
 	connectionAddress.sin_port = htons(myPort);
-	// initialize the server socket
+	
 	int result;
-	WSADATA wsaData; // gets populated w/ info explaining this sockets implementation
-	// load Winsock 2.0 DLL. initiates use of the Winsock DLL by a process
+	WSADATA wsaData; 
+	
 	if ((result = WSAStartup(MAKEWORD(1, 1), &wsaData)) != 0)
 	{
 		printf("WSAStartup() error %d\n", result);
 		return EXIT_FAILURE;
 	}
-	atexit(finalWSACleanup); // add callback to trigger when program ends. cleans up sockets
-	// create the main socket, either client or server
+	atexit(finalWSACleanup); 
+	
 	SOCKET mySocket = socket(
-		/*
-		AF_UNSPEC: 0     The address family is unspecified.
-		AF_INET: 2       The Internet Protocol version 4 (IPv4) address family.
-		AF_IPX: 6        The IPX/SPX address family. This address family is only supported if the NWLink IPX/SPX NetBIOS Compatible Transport protocol is installed. This address family is not supported on Windows Vista and later.
-		AF_APPLETALK: 17 The AppleTalk address family. This address family is only supported if the AppleTalk protocol is installed. This address family is not supported on Windows Vista and later.
-		AF_NETBIOS: 17   The NetBIOS address family. This address family is only supported if a Windows Sockets provider for NetBIOS is installed.
-		AF_INET6: 23     The Internet Protocol version 6 (IPv6) address family.
-		AF_IRDA: 26      The Infrared Data Association (IrDA) address family. This address family is only supported if the computer has an infrared port and driver installed.
-		AF_BTH: 32       The Bluetooth address family. This address family is supported on Windows XP with SP2 or later if the computer has a Bluetooth adapter and driver installed.
-		*/
+		
 		AF_INET,
-		/*
-		SOCK_STREAM: 1    A socket type that provides sequenced, reliable, two-way, connection-based byte streams with an OOB data transmission mechanism. This socket type uses the Transmission Control Protocol (TCP) for the Internet address family (AF_INET or AF_INET6).
-		SOCK_DGRAM: 2     A socket type that supports datagrams, which are connectionless, unreliable buffers of a fixed (typically small) maximum length. This socket type uses the User Datagram Protocol (UDP) for the Internet address family (AF_INET or AF_INET6).
-		SOCK_RAW: 3       A socket type that provides a raw socket that allows an application to manipulate the next upper-layer protocol header. To manipulate the IPv4 header, the IP_HDRINCL socket option must be set on the socket. To manipulate the IPv6 header, the IPV6_HDRINCL socket option must be set on the socket.
-		SOCK_RDM: 4       A socket type that provides a reliable message datagram. An example of this type is the Pragmatic General Multicast (PGM) multicast protocol implementation in Windows, often referred to as reliable multicast programming. This type is only supported if the Reliable Multicast Protocol is installed.
-		SOCK_SEQPACKET: 5 A socket type that provides a pseudo-stream packet based on datagrams.
-		*/
+		
 		SOCK_STREAM,
-		/*
-		BTHPROTO_RFCOMM: 3 The Bluetooth Radio Frequency Communications (Bluetooth RFCOMM) protocol. This is a possible value when the af parameter is AF_BTH and the type parameter is SOCK_STREAM. This protocol is supported on Windows XP with SP2 or later.
-		IPPROTO_TCP: 6     The Transmission Control Protocol (TCP). This is a possible value when the af parameter is AF_INET or AF_INET6 and the type parameter is SOCK_STREAM.
-		IPPROTO_UDP: 17    The User Datagram Protocol (UDP). This is a possible value when the af parameter is AF_INET or AF_INET6 and the type parameter is SOCK_DGRAM.
-		IPPROTO_RM: 113    The PGM protocol for reliable multicast. This is a possible value when the af parameter is AF_INET and the type parameter is SOCK_RDM. On the Windows SDK released for Windows Vista and later, this value is also called IPPROTO_PGM. This protocol is only supported if the Reliable Multicast Protocol is installed.
-		*/
+		
 		IPPROTO_TCP);
 	if (mySocket == INVALID_SOCKET)
 	{
 		printf("socket() error %d\n", WSAGetLastError());
 		return EXIT_FAILURE;
 	}
-	// 1 to set non-blocking, 0 to set re-usable
+	
 	unsigned long argp = 1;
-	// for complete info on this method, check out http://msdn.microsoft.com/en-us/library/ms740476(VS.85).aspx
+	
 	result = setsockopt(mySocket,
 		SOL_SOCKET,
-		/*
-		SO_REUSEADDR: 4 Allows the socket to be bound to an address that is already in use. For more information, see bind. Not applicable on ATM sockets.
-		*/
+		
 		SO_REUSEADDR,
 		(char *)&argp, sizeof(argp));
 	if (result != 0)
@@ -77,22 +55,18 @@ int main()
 		printf("setsockopt() error %d\n", result);
 		return EXIT_FAILURE;
 	}
-	// 1 to set non-blocking, 0 to set blocking
+	
 	argp = 1;
-	// attempt to setup the socket as non-blocking
+	
 	if (ioctlsocket(mySocket,
-		/*
-		FIONBIO:    The *argp parameter is a pointer to an unsigned long value. Set *argp to a nonzero value if the nonblocking mode should be enabled, or zero if the nonblocking mode should be disabled. When a socket is created, it operates in blocking mode by default (nonblocking mode is disabled). This is consistent with BSD sockets.
-		FIONREAD:   Use to determine the amount of data pending in the network's input buffer that can be read from socket s. The argp parameter points to an unsigned long value in which ioctlsocket stores the result. FIONREAD returns the amount of data that can be read in a single call to the recv function, which may not be the same as the total amount of data queued on the socket. If s is message oriented (for example, type SOCK_DGRAM), FIONREAD still returns the amount of pending data in the network buffer, however, the amount that can actually be read in a single call to the recv function is limited to the data size written in the send or sendto function call.
-		SIOCATMARK: Use to determine if all out of band (OOB) data has been read. (See Windows Sockets 1.1 Blocking Routines and EINPROGRESS for a discussion on OOB data.) This applies only to a stream oriented socket (for example, type SOCK_STREAM) that has been configured for in-line reception of any OOB data (SO_OOBINLINE). On sockets with the SO_OOBINLINE socket option set, SIOCATMARK always returns TRUE and the OOB data is returned to the user as normal data.
-		*/
+		
 		FIONBIO,
 		&argp) == SOCKET_ERROR)
 	{
 		printf("ioctlsocket() error %d\n", WSAGetLastError());
 		return EXIT_FAILURE;
 	}
-	// make the decision about client or server socket here.
+	
 	result = serverLogic(mySocket, (sockaddr*)&connectionAddress);
 	if (result == EXIT_FAILURE)
 	{
@@ -100,7 +74,7 @@ int main()
 	}
 	if (mySocket != INVALID_SOCKET)
 	{
-		result = closesocket(mySocket); // server closes, it doesn't shut down
+		result = closesocket(mySocket); 
 		if (result != 0)
 		{
 			printf("closesocket() error %d\n", WSAGetLastError());
@@ -113,35 +87,35 @@ int main()
 
 int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 {
-	// start listening on the server
+	
 	int result = bind(mySocket, connectionAddress, sizeof(sockaddr_in));
 	if (result == SOCKET_ERROR)
 	{
 		printf("bind() error %d\n", WSAGetLastError());
 		return EXIT_FAILURE;
 	}
-	result = listen(mySocket, /* size of connection queue */10);
+	result = listen(mySocket, 10);
 	if (result == SOCKET_ERROR)
 	{
 		printf("listen() error %d\n", WSAGetLastError());
 		return EXIT_FAILURE;
 	}
-	// keep track of client connections
+	
 	struct ClientSocket {
 		SOCKET socket;
 		sockaddr address;
 	};
-	// used to broker connection with clients
+	
 	std::vector<ClientSocket> clientSockets;
-	// keep track of messages read from clients
+	
 	std::vector<char> inputBuffer;
-	// server loop
+	
 	int iterations = 0;
 	while ((result = getReadStatus(mySocket)) != SOCKET_ERROR)
 	{
-		if (_kbhit() && _getch() == 27)  // escape key press
+		if (_kbhit() && _getch() == 27)  
 		{
-			break; // end the server loop
+			break; 
 		}
 		if (result == 1)
 		{
@@ -161,7 +135,7 @@ int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 		else
 		{
 			printf("server: %d\r", iterations++);
-			// read all the data from the clients
+			
 			inputBuffer.clear();
 			for (unsigned int i = 0; i < clientSockets.size(); ++i)
 			{
@@ -181,9 +155,9 @@ int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 						inputBuffer.resize(used + howMuchInBuffer, 0);
 						result = recv(clientSockets[i].socket,
 							(char*)(&inputBuffer[used]), howMuchInBuffer, 0);
-						// NOTE: for multi-byte values sent across the network
-						// ntohl or ntohs should be used to convert 4-byte and
-						// 2-byte values respectively.
+						
+						
+						
 						if (result == SOCKET_ERROR)
 						{
 							printf("client %d recv() error %d\n", i,
@@ -205,26 +179,26 @@ int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 			}
 			if (inputBuffer.size() > 0)
 			{
-				// print data about to be sent
+				
 				printf("sending: \"");
 				for (unsigned int i = 0; i < inputBuffer.size(); ++i)
 				{
 					printf("%c", inputBuffer[i]);
 				}
 				printf("\" %d bytes\n", inputBuffer.size());
-				// send all known data to all clients
+				
 				for (unsigned int i = 0; i < clientSockets.size(); ++i)
 				{
 					if (clientSockets[i].socket == INVALID_SOCKET)
 					{
 						continue;
 					}
-					// NOTE: for multi-byte values sent across the network
-					// htonl or htons should be used to convert 4-byte and
-					// 2-byte values respectively.
+					
+					
+					
 					result = send(clientSockets[i].socket,
 						(const char*)&inputBuffer[0], inputBuffer.size(), 0);
-					if (result == SOCKET_ERROR) // if the socket failed
+					if (result == SOCKET_ERROR) 
 					{
 						printf("\rclient %d send error %d\n", i,
 							WSAGetLastError());
@@ -237,7 +211,7 @@ int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 					}
 				}
 			}
-			// removed released sockets from list, backwards to avoid skips
+			
 			if (clientSockets.size() > 0)
 			{
 				for (int i = (signed)clientSockets.size() - 1; i >= 0; --i)
@@ -250,7 +224,7 @@ int serverLogic(SOCKET mySocket, sockaddr * connectionAddress)
 			}
 		}
 	}
-	// release all client sockets
+	
 	for (unsigned int i = 0; i < clientSockets.size(); ++i)
 	{
 		int result = endBrokerSocket(clientSockets[i].socket);
@@ -282,12 +256,10 @@ int endBrokerSocket(SOCKET socket)
 
 int getReadStatus(const SOCKET a_socket)
 {
-	// zero seconds, zero milliseconds. max time select call allowd to block
+	
 	static const timeval instantSpeedPlease = { 0, 0 };
 	fd_set a = { 1, { a_socket } };
-	/*
-	select returns the number of ready socket handles in the fd_set structure, zero if the time limit expired, or SOCKET_ERROR if an error occurred. WSAGetLastError can be used to retrieve a specific error code.
-	*/
+	
 	int result = select(0, &a, 0, 0, &instantSpeedPlease);
 	if (result == SOCKET_ERROR)
 	{
@@ -301,7 +273,7 @@ int getReadStatus(const SOCKET a_socket)
 	return result;
 }
 
-void finalWSACleanup() // callback used to clean up sockets
+void finalWSACleanup() 
 {
 	int result = WSACleanup();
 	if (result != 0)
